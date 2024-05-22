ad04bb23d962f02f563257147511f70ee5fd7619
ch4rv1ss <xavirs10@gmail.com>
2024-05-21 20:53:53 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -10,7 +10,7 @@ let mensaje = document.getElementById("mensaje");
 function callApi(estatus){
     // Hacer una petición para un usuario con ID especifico
 axios
-  .get('https://18.191.49.123/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
+  .get('https://3.136.11.19/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
   .then(function (response) {
     // manejar respuesta exitosa
     console.log(response);
